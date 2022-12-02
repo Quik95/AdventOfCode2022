b433b81cbb056eb6a69fc85e90710bc5f2f14452
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-12-02 18:14:37 +0100

    Day 02

---
+++ AdventOfCode2022.Solvers/Day02.cs
@@ -0,0 +1,53 @@
+namespace AdventOfCode2022.Solvers;
+
+public sealed class Day02 : BaseDay
+{
+    private static readonly Dictionary<string, int> LookupTable = new()
+    {
+        {"A X", 1 + 3},
+        {"A Y", 2 + 6},
+        {"A Z", 3 + 0},
+        {"B X", 1 + 0},
+        {"B Y", 2 + 3},
+        {"B Z", 3 + 6},
+        {"C X", 1 + 6},
+        {"C Y", 2 + 0},
+        {"C Z", 3 + 3}
+    };
+
+    private static readonly Dictionary<string, int> LookupTable2 = new()
+    {
+        {"A X", 3 + 0},
+        {"A Y", 1 + 3},
+        {"A Z", 2 + 6},
+        {"B X", 1 + 0},
+        {"B Y", 2 + 3},
+        {"B Z", 3 + 6},
+        {"C X", 2 + 0},
+        {"C Y", 3 + 3},
+        {"C Z", 1 + 6}
+    };
+
+    private readonly string[] _input;
+
+    public Day02()
+    {
+        _input = ParseInput();
+    }
+
+    public Day02(string input) : base(input)
+    {
+        _input = ParseInput();
+    }
+
+
+    public override int SolvePart1()
+    {
+        return _input.Aggregate(0, (cumulative, next) => cumulative + LookupTable[next]);
+    }
+
+    public override int SolvePart2()
+    {
+        return _input.Aggregate(0, (cumulative, next) => cumulative + LookupTable2[next]);
+    }
+}
--- AdventOfCode2022.Tests/UnitTest1.cs
+++ AdventOfCode2022.Tests/UnitTest1.cs
@@ -1,9 +1,23 @@
+using AdventOfCode2022.Solvers;
+
 namespace AdventOfCode2022.Tests;
 
-public class UnitTest1
+public class AdventOfCodeTests
 {
-    [Fact]
-    public void Test1()
+    public class Day02Tests
     {
+        [Fact]
+        public void Part1Example()
+        {
+            var d = new Day02("A Y\nB X\nC Z");
+            Assert.Equal(15, d.SolvePart1());
+        }
+
+        [Fact]
+        public void Part2Example()
+        {
+            var d = new Day02("A Y\nB X\nC Z");
+            Assert.Equal(12, d.SolvePart2());
+        }
     }
 }
